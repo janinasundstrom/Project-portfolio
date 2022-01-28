5211dd7ba0238df6db9f3257c6e3dc89096764b1
Janina Sundström <janina.sundstrom@chasacademy.se>
2022-01-28 09:24:49 +0100

    button

--- main.js
+++ main.js
@@ -2,3 +2,10 @@ function revealMessage() {
     document.getElementById("hiddenMessage").style.display = 'block';
 
 }
+
+
+
+function changeColor(){
+    document.querySelector(".javabox").style.color='blue';
+
+}
